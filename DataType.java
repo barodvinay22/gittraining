c5d9483cf01d5c86edb90c54257d648465f63b7d
Vinay Barod <vbarod@mobiquityinc.com>
2026-04-09 11:54:48 +0530

    New File Added and Hello modified

---
+++ DataType.java
@@ -0,0 +1,15 @@
+class DataTypes{
+    public static void main(String...a){
+        int salary = 1000;
+        Integer salary1 =1000;
+        String empName="Vinay";
+        
+    }
+}
+
+
+// DataTypes 
+// Primitive int float char='A' 'Z'  double
+// Non Primitive String Arrays Objects
+
+// Wrapper Class --> Integer Float Character Double 
